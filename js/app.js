3ac8259ec4d3325fe1108c5eb51458ff10fca1a3
Andrew Keith <38077188+crankeith@users.noreply.github.com>
2019-01-30 08:41:09 -0500

    Updated tshirt design section to use regex for flexibility Added some starting code and comments for the Activities section

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
 const color = $('#color');
-const tShirtColors = {
-    'js puns': ["cornflowerblue", "darkslategrey", "gold"],
-    'heart js': ["tomato", "steelblue", "dimgrey"]
-};
 
 //Set focus to the first field on page load
 $( window ).on('load', function() {
@@ -19,29 +15,55 @@ $('#title').on('change', (e) => {
     }
 });
 
+//----------
+// TSHIRT DESIGN
+//----------
+
 //Initially disable color selector until theme is chosen
 color.attr('disabled', true);
 
 //Change event handler for tshirt design
 $('#design').on('change', (e) => {
-    const theme = e.target.value;
+    const selectedTheme = e.target.selectedOptions[0].textContent;
     const colors = color.children();
+    //Create regex by removing beginning theme text
+    const regex = new RegExp(selectedTheme.replace('Theme - ', ''));
 
     //Reset color dropdown selection
     color.val('');
 
-    //Loop through colors and if theme matches object key, hide/show colors based on array
-    for(let i = 0; i < colors.length; i++){
-        if(tShirtColors[theme]){
-            color.attr('disabled', false);
-            if(tShirtColors[theme].indexOf(colors[i].value) === -1){
-                $(colors[i]).hide();
-            } else{
-                $(colors[i]).show();
+    //Loop through colors and if the regex matches, hide/show colors based on array
+    if(selectedTheme !== 'Select Theme'){
+        color.attr('disabled', false);
+        for(let i = 0; i < colors.length; i++){
+                if(regex.test(colors[i].textContent)){
+                    $(colors[i]).show();
+                } else{
+                    $(colors[i]).hide();
+                }
             }
-        } else {
-            color.attr('disabled', true);
         }
+    else {
+        color.attr('disabled', true);
     }
 });
+
+//----------
+// ACTIVITIES
+//----------
+//Some sample code/regex to retrieve the day/time for comparison
+const dayTimeRegex = /— [\w\s\d-]+/;
+const priceRegex = /\$\d+/;
+const dayTime =  "JavaScript Frameworks Workshop — Tuesday 9am-12pm, $100".match(dayTimeRegex); //returns an array
+const price =  "JavaScript Frameworks Workshop — Tuesday 9am-12pm, $100".match(priceRegex);
+const dayTimeChosen = dayTime[0];
+const priceChosen = Number(price[0].replace(/\$/,''));
+console.log(dayTimeChosen);
+console.log(priceChosen);
+//Add event listener for change? that will disable checkboxes based on function
+//Create function to use when looping through each item to hide ones that match "value"
+//Create total element in Javascript and append
+//Create variable to store running total and a function to update total element value
+
+
 
